a3b01c910b1cccb5dca85b4dd2341fee36bf6ed1
kolev-sf <kole.vasilevski@scalefocus.com>
2025-04-29 16:37:19 +0200

    Layout changes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Meddon } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const meddon = Meddon({
+  weight: ['400'],
+  subsets: ['latin'],
+});
